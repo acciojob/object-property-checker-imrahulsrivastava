72081ab471cb0c2a4cec5f8751ce7c1668ec1a67
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-10-12 00:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+// Write your code here
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF",
+};
 
 function hasKey(key) {
   //   write your code here
+  return key in sampleObject;
 }
 
 // Do not change the code below
